e59476724488243f3e233f051d9166e092e89b51
theocasagrande <theozito.casagrande@gmail.com>
2025-11-17 19:39:06 -0300

    audio funcionando

--- js/tela_2_theo.js
+++ js/tela_2_theo.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     const texto = document.querySelector('.textotela2')
+    const play = document.querySelector('.imagem')
 
     const stored = localStorage.getItem('textofinal')
     if (!stored) {
@@ -12,6 +13,43 @@ document.addEventListener('DOMContentLoaded', function() {
     const partes = stored.split(',').filter(p => p !== '')
     const frase = partes.join(' ')
     texto.innerHTML = frase
+
+
+
+    let voiceBR = null
+
+    function carregarVozes() {
+        const voices = speechSynthesis.getVoices();
+        voiceBR = voices.find(v => v.lang === 'pt-BR');
+
+        play.addEventListener('click', () => {
+            const u = new SpeechSynthesisUtterance(frase)
+            u.rate = 1
+            u.pitch = 1
+            u.voice = voiceBR
+            speechSynthesis.speak(u)
+        })
+    }
+    speechSynthesis.onvoiceschanged = carregarVozes
+    
+
+    play.style.transition = "transform 0.1s ease"
+
+    play.addEventListener("mousedown", () => {
+        play.style.transform = "scale(0.93)"
+    })
+    play.addEventListener("mouseup", () => {
+        play.style.transform = ""
+    })
+    play.addEventListener("mouseleave", () => {
+        play.style.transform = ""
+    })
+    play.addEventListener("touchstart", () => {
+        play.style.transform = "scale(0.93)";
+    })
+    play.addEventListener("touchend", () => {
+        play.style.transform = ""
+    })
 
 
 
